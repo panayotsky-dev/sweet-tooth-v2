a0c31607be137689ccd2886ee0a37b1daab32132
Panayotsky-dev <panayotsky.dev@gmail.com>
2023-10-17 16:56:40 +0300

    react-navigation/native

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import {  Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
 
 export default function App() {
   return (
+    <NavigationContainer>
     <View className="flex-1 items-center justify-center bg-black" >
       <Text  className="text-red-500">Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+    </NavigationContainer>
   );
 }
 
